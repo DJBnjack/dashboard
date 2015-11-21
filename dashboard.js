f32bbcf810f4fc04087df5b6b8cdd68cadd5ddb9
Johannes Bertens <johannes.bertens@live.nl>
2015-11-21 19:27:06 +0100

    Using socket.io now for updates

--- dashboard.js
+++ dashboard.js
@@ -2,24 +2,49 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var client = require('socket.io-client')('http://localhost:3001');
 var redis = require('socket.io-redis');
+var request = require('request');
+
+// IO adapter to get info from other senders
 io.adapter(redis({ host: 'redis.core.djbnjack.svc.tutum.io', port: 6379 }));
 
 // Routing
 app.use(express.static(__dirname + '/public'));
 
-setInterval(() => io.emit('system', 'ping from server'), 5000);
+var processJson = "";
+var updateProcesses = function(callback) {
+    var url = "http://processes-api.core.djbnjack.svc.tutum.io:3000/processes";
+    // var url = "http://localhost:3000/processes";
+    request(url, function (error, response, body) {
+        if(error){
+            console.log('Error:', error);
+        } else if(response.statusCode !== 200){
+            console.log('Invalid Status Code Returned:', response.statusCode);
+        }
+    
+        processJson = body;
+        if (callback != null) callback(processJson);
+    });    
+}
+
+// Initial load of processes
+updateProcesses();
 
 // Socket logic
 io.on('connection', function(socket){
-    console.log('user connected');
-	socket.emit('system', 'hello, user!');
-	
-    
-	socket.on('chat message', function(msg){
-		console.log('message: ' + msg);
-		io.emit('chat message', msg);
-	});
+	socket.emit('processes', processJson);
+});
+
+client.on('updated', function(msg){
+    console.log('updated: ' + msg);
+    if (msg == 'processes') {
+        updateProcesses((updatedProcesses) => io.emit('processes', updatedProcesses));
+    }
+});
+
+client.on('system', function(msg){
+    console.log('system: ' + msg);
 });
 
 // Listen
--- public/controller.js
+++ public/controller.js
@@ -34,19 +34,10 @@ processApp.factory('socket', ['$rootScope', function ($rootScope) {
 
 processApp.controller('ProcessListCtrl', function ($scope, $http, socket) {
 	$scope.processes = [];
-    $scope.recievedTroughSocket = "";
     
-	var url = "http://processes-api.core.djbnjack.svc.tutum.io:3000/processes";
-	// var url = "http://localhost:3000/processes";
-	$http.get(url)
-		.then(
-			response => $scope.processes = response.data.map(element => element.row[0]), 
-			error => console.log(error)
-		);
-	
-    var logData = function(data) {
-        $scope.recievedTroughSocket += "[" + new Date().toLocaleString() + "] " + data + "\n";
+    var updateProcesses = function(data) {
+        $scope.processes = JSON.parse(data).map(element => element.row[0]);
     }
     
-	socket.on('system', logData);
+    socket.on('processes', updateProcesses);
 });
