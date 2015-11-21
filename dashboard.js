bec7a4b76f353d53262208b5a7422203761efb34
Johannes Bertens <johannes.bertens@live.nl>
2015-11-21 18:36:25 +0100

    Added redis IO integration

--- dashboard.js
+++ dashboard.js
@@ -2,13 +2,19 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var redis = require('socket.io-redis');
+io.adapter(redis({ host: 'redis.core.djbnjack.svc.tutum.io', port: 6379 }));
 
 // Routing
 app.use(express.static(__dirname + '/public'));
 
+setInterval(() => io.emit('system', 'ping from server'), 5000);
+
 // Socket logic
 io.on('connection', function(socket){
-    console.log('connected');
+    console.log('user connected');
+	socket.emit('system', 'hello, user!');
+	
     
 	socket.on('chat message', function(msg){
 		console.log('message: ' + msg);
--- public/controller.js
+++ public/controller.js
@@ -1,7 +1,41 @@
 var processApp = angular.module('processApp', []);
 
-processApp.controller('ProcessListCtrl', function ($scope, $http) {
+processApp.factory('socket', ['$rootScope', function ($rootScope) {
+    var socket = io.connect();
+ 
+    return {
+        on: function (eventName, callback) {
+            function wrapper() {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    callback.apply(socket, args);
+                });
+            }
+ 
+            socket.on(eventName, wrapper);
+ 
+            return function () {
+                socket.removeListener(eventName, wrapper);
+            };
+        },
+ 
+        emit: function (eventName, data, callback) {
+            socket.emit(eventName, data, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    if(callback) {
+                        callback.apply(socket, args);
+                    }
+                });
+            });
+        }
+    };
+}]);
+
+processApp.controller('ProcessListCtrl', function ($scope, $http, socket) {
 	$scope.processes = [];
+    $scope.recievedTroughSocket = "";
+    
 	var url = "http://processes-api.core.djbnjack.svc.tutum.io:3000/processes";
 	// var url = "http://localhost:3000/processes";
 	$http.get(url)
@@ -9,4 +43,10 @@ processApp.controller('ProcessListCtrl', function ($scope, $http) {
 			response => $scope.processes = response.data.map(element => element.row[0]), 
 			error => console.log(error)
 		);
+	
+    var logData = function(data) {
+        $scope.recievedTroughSocket += "[" + new Date().toLocaleString() + "] " + data + "\n";
+    }
+    
+	socket.on('system', logData);
 });
